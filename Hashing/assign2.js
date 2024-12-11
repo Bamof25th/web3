0556a50e32c902c8c8d82bac6addc5205789f5bf
Aniket Baghel <88363603+Bamof25th@users.noreply.github.com>
2024-12-11 21:48:52 +0530

    assignment 2 done

--- Hashing/assign2.js
+++ Hashing/assign2.js
@@ -1,13 +1,16 @@
 const crypto = require("crypto");
 
-//  find a number which hashes to first four strings as 0000
+//  find a number which hashes to first four strings as 0000 and starts with "100xdevs"
 
 function findHash(start, end) {
   const target = "0000";
   for (let i = start; i <= end; i++) {
-    const hash = crypto.createHash("sha256").update(String(i)).digest("hex");
+    const hash = crypto
+      .createHash("sha256")
+      .update(String("100xdevs" + i))
+      .digest("hex");
     if (hash.slice(0, 4) === target) {
-      return  {input: i, hash: hash };
+      return { input: i, hash: hash };
     }
   }
   return -1;
